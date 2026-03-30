b6f49ee85254638c6d0e53d45ef82f8352a60ab3
S.M.S.Udesha <shamithud20@gmail.com>
2026-03-30 21:13:44 +0530

    "Create Database"

---
+++ Supplier_Service/database.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client.Pharamcy_Sys_Suppliers
+supplier_collection = database.supplier
