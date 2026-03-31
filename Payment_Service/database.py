9472dc475eabff6d280a1789c8a72ca1b2647ba4
Buddhika Roshan <156290201+BuddhikaRoshan@users.noreply.github.com>
2026-03-31 20:24:52 +0530

    Create Database.py
    create database connection and its parts

---
+++ Payment_Service/database.py
@@ -0,0 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client.Pharamcy_Sys_Payments
+payment_collection = database.payment
