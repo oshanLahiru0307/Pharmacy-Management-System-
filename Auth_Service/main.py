708d32866ac4043eaad7aba20885ba569f0016f4
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-04-03 17:08:54 +0530

    main.py file is updated

---
+++ Auth_Service/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from auth_services import router
+
+app = FastAPI(title="Auth Microservice")
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8008,
+        reload=True  
+    )
